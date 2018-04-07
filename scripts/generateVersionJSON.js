647e9dcd6bfb3d23067a29eb21dfde1ad93c5fde
John Karahalis <john.karahalis@gmail.com>
2018-04-07 01:26:01 -0400

    Fix eslint errors

--- scripts/generateVersionJSON.js
+++ scripts/generateVersionJSON.js
@@ -7,15 +7,17 @@ const packageJSON = require('../package.json');
 const outFilename = './build/version.json';
 
 const versionJSON = {
-    source: "https://github.com/mozilla/ensemble-transposer",
+    source: 'https://github.com/mozilla/ensemble-transposer',
     version: packageJSON.version,
     commit: process.env.SOURCE_VERSION || childProcess.execSync('git rev-parse HEAD').toString().trim(),
 };
 
 fs.writeFile(outFilename, JSON.stringify(versionJSON, null, 4), error => {
     if (error) {
+        // eslint-disable-next-line no-console
         console.error(error);
     }
 
+    // eslint-disable-next-line no-console
     console.log('Wrote ' + outFilename);
 });
