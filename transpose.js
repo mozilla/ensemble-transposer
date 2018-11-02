ca0e02f33332aea5d44a10e039defae603ace77d
John Karahalis <john.karahalis@gmail.com>
2018-11-02 16:20:00 -0400

    Remove support for local data sources
    Before the Firefox Public Data Report was launched, much of the data
    that ensemble-transposer ingested was secret. To keep it secret, we
    downloaded it locally and added support for local data sources.
    
    All of the data that ensemble-transposer ingests is now public, so there
    is no longer a need for this support. If we do need to work with secret
    data in the future, we could just host it locally with the "serve" npm
    module (or similar) to keep things simple.

--- transpose.js
+++ transpose.js
@@ -1,17 +1,150 @@
-const fs = require('fs');
 const request = require('request');
 
 
 module.exports = (manifest, callback) => {
-    if (isURL(manifest.sources.data)) {
-        request(manifest.sources.data, (error, response, body) => processData(error, body, manifest, callback));
-    } else {
-        fs.readFile(manifest.sources.data, 'utf8', (error, body) => processData(error, body, manifest, callback));
-    }
-}
+    request(manifest.sources.data, (error, response, body) => {
+        if (error) {
+            return callback({
+                error: `Error getting ${manifest.sources.data}`,
+            });
+        }
 
-function isURL(str) {
-    return str.startsWith('http');
+        const sourceData = JSON.parse(body);
+        const dataset = new Dataset(
+            manifest.extraMetadata.title,
+            manifest.extraMetadata.description,
+            manifest.extraMetadata.defaultCategory,
+        );
+
+        if (manifest.extraMetadata.summaryMetrics) {
+            dataset.addSummaryMetrics(manifest.extraMetadata.summaryMetrics);
+        }
+
+        // Add sections to dataset object if they have been defined
+        const sectioned = manifest.extraMetadata.dashboard.sectioned;
+        if (sectioned) {
+            manifest.extraMetadata.dashboard.sections.forEach(sectionMeta => {
+                const key = sectionTitleToKey(sectionMeta.title);
+                dataset.addSection(new Section(key, sectionMeta.title, sectionMeta.metrics));
+            });
+        }
+
+        if (manifest.sources.annotations) {
+            request(manifest.sources.annotations, (error, response, body) => processCategories(body));
+        } else {
+            processCategories();
+        }
+
+        function sectionTitleToKey(sectionTitle) {
+            return sectionTitle.replace(' ', '').toLowerCase();
+        }
+
+        function processCategories(annotationsBody) {
+            let sourceAnnotations;
+
+            if (annotationsBody) {
+                sourceAnnotations = JSON.parse(annotationsBody);
+            }
+
+            // For each category NAME, where a category looks like:
+            //
+            // "US": {...}
+            Object.keys(sourceData).forEach(categoryName => {
+                dataset.addCategoryName(categoryName);
+
+                // For each entry OBJECT in that category, where an entry looks like:
+                //
+                // {
+                //     "date": "2018-01-01",
+                //     "metrics": {...}
+                // }
+                sourceData[categoryName].forEach(entry => {
+                    dataset.addDate(entry.date);
+
+                    let metricsToShow;
+                    if (sectioned) {
+                        metricsToShow = manifest.extraMetadata.dashboard.sections.reduce((acc, sectionMeta) => {
+                            return acc.concat(sectionMeta.metrics);
+                        }, []);
+                    } else {
+                        metricsToShow = manifest.extraMetadata.dashboard.metrics;
+                    }
+
+                    // For each metric NAME to show:
+                    //
+                    // (By consequence, anything not named in the "dashboards" portion
+                    // of the manifest will not be part of the final output. Also, the
+                    // ordering of the final output will match the ordering of metrics
+                    // in the manifest.)
+                    metricsToShow.forEach(metricSlug => {
+                        const metricMeta = manifest.extraMetadata.metrics[metricSlug];
+                        const metricType = metricMeta.type;
+
+                        const metric = dataset.getMetric(
+                            metricSlug,
+                            metricSlugToTitle(metricSlug, manifest.extraMetadata),
+                            metricMeta.description,
+                            metricType,
+                            metricMeta.axes,
+                            metricMeta.columns,
+                        );
+
+                        // If this metric is not in this entry, do nothing else. We need
+                        // to call getMetric() before bailing out here, otherwise the
+                        // ordering of metrics would be messed up in the final output.
+                        if (!propertyExists(entry.metrics, metricSlug)) return;
+
+                        const categoryData = metric.getCategoryData(categoryName);
+
+                        // Add annotations if any
+                        if (sourceAnnotations) {
+                            sourceAnnotations[categoryName].forEach(sa => {
+                                if (propertyExists(sa.annotation, metricSlug)) {
+                                    const annotations = metric.getCategoryAnnotations(categoryName);
+                                    annotations.addAnnotation(sa.date, sa.annotation[metricSlug]);
+                                }
+                            });
+                        }
+
+                        if (metricType === 'line') {
+
+                            // If the source dataset doesn't specify any populations, create
+                            // one "default" population.
+                            let populations;
+                            const multiplePopulations = typeof(entry.metrics[metricSlug]) === 'object';
+                            if (multiplePopulations) {
+                                populations = Object.keys(entry.metrics[metricSlug]);
+                            } else {
+                                populations = ['default'];
+                            }
+
+                            // For each population NAME
+                            populations.forEach(populationName => {
+                                const population = categoryData.getPopulation(populationName);
+                                const yValue = multiplePopulations ? entry.metrics[metricSlug][populationName] : entry.metrics[metricSlug];
+                                const dataPoint = new DataPoint(entry.date, yValue);
+                                population.addDataPoint(dataPoint);
+                            }); // For each population NAME
+
+                        } else if (metricType === 'table') {
+                            // For each row NAME
+                            Object.keys(entry.metrics[metricSlug]).map(rowName => {
+                                const tableSnapshot = categoryData.getTableSnapshot(entry.date);
+                                const row = new Row(rowName, entry.metrics[metricSlug][rowName]);
+                                tableSnapshot.addRow(row);
+                            }); // For each row NAME
+                        }
+
+                    }); // For each metric NAME
+
+                }); // For each entry OBJECT
+
+            }); // For each category NAME
+
+            callback(dataset.render());
+
+        } // processCategories
+    });
 }
 
 function propertyExists(obj, property) {
@@ -26,149 +159,6 @@ function metricSlugToTitle(metricSlug, extraMetadata) {
     return extraMetadata.metrics[metricSlug].title || metricSlug;
 }
 
-function processData(error, dataBody, manifest, callback) {
-    if (error) {
-        return callback({
-            error: `Error getting ${manifest.sources.data}`,
-        });
-    }
-
-    const sourceData = JSON.parse(dataBody);
-    const dataset = new Dataset(
-        manifest.extraMetadata.title,
-        manifest.extraMetadata.description,
-        manifest.extraMetadata.defaultCategory,
-    );
-
-    if (manifest.extraMetadata.summaryMetrics) {
-        dataset.addSummaryMetrics(manifest.extraMetadata.summaryMetrics);
-    }
-
-    // Add sections to dataset object if they have been defined
-    const sectioned = manifest.extraMetadata.dashboard.sectioned;
-    if (sectioned) {
-        manifest.extraMetadata.dashboard.sections.forEach(sectionMeta => {
-            const key = sectionTitleToKey(sectionMeta.title);
-            dataset.addSection(new Section(key, sectionMeta.title, sectionMeta.metrics));
-        });
-    }
-
-    if (manifest.sources.annotations) {
-        request(manifest.sources.annotations, (error, response, body) => processCategories(body));
-    } else {
-        processCategories();
-    }
-
-    function sectionTitleToKey(sectionTitle) {
-        return sectionTitle.replace(' ', '').toLowerCase();
-    }
-
-    function processCategories(annotationsBody) {
-        let sourceAnnotations;
-
-        if (annotationsBody) {
-            sourceAnnotations = JSON.parse(annotationsBody);
-        }
-
-        // For each category NAME, where a category looks like:
-        //
-        // "US": {...}
-        Object.keys(sourceData).forEach(categoryName => {
-            dataset.addCategoryName(categoryName);
-
-            // For each entry OBJECT in that category, where an entry looks like:
-            //
-            // {
-            //     "date": "2018-01-01",
-            //     "metrics": {...}
-            // }
-            sourceData[categoryName].forEach(entry => {
-                dataset.addDate(entry.date);
-
-                let metricsToShow;
-                if (sectioned) {
-                    metricsToShow = manifest.extraMetadata.dashboard.sections.reduce((acc, sectionMeta) => {
-                        return acc.concat(sectionMeta.metrics);
-                    }, []);
-                } else {
-                    metricsToShow = manifest.extraMetadata.dashboard.metrics;
-                }
-
-                // For each metric NAME to show:
-                //
-                // (By consequence, anything not named in the "dashboards" portion
-                // of the manifest will not be part of the final output. Also, the
-                // ordering of the final output will match the ordering of metrics
-                // in the manifest.)
-                metricsToShow.forEach(metricSlug => {
-                    const metricMeta = manifest.extraMetadata.metrics[metricSlug];
-                    const metricType = metricMeta.type;
-
-                    const metric = dataset.getMetric(
-                        metricSlug,
-                        metricSlugToTitle(metricSlug, manifest.extraMetadata),
-                        metricMeta.description,
-                        metricType,
-                        metricMeta.axes,
-                        metricMeta.columns,
-                    );
-
-                    // If this metric is not in this entry, do nothing else. We need
-                    // to call getMetric() before bailing out here, otherwise the
-                    // ordering of metrics would be messed up in the final output.
-                    if (!propertyExists(entry.metrics, metricSlug)) return;
-
-                    const categoryData = metric.getCategoryData(categoryName);
-
-                    // Add annotations if any
-                    if (sourceAnnotations) {
-                        sourceAnnotations[categoryName].forEach(sa => {
-                            if (propertyExists(sa.annotation, metricSlug)) {
-                                const annotations = metric.getCategoryAnnotations(categoryName);
-                                annotations.addAnnotation(sa.date, sa.annotation[metricSlug]);
-                            }
-                        });
-                    }
-
-                    if (metricType === 'line') {
-
-                        // If the source dataset doesn't specify any populations, create
-                        // one "default" population.
-                        let populations;
-                        const multiplePopulations = typeof(entry.metrics[metricSlug]) === 'object';
-                        if (multiplePopulations) {
-                            populations = Object.keys(entry.metrics[metricSlug]);
-                        } else {
-                            populations = ['default'];
-                        }
-
-                        // For each population NAME
-                        populations.forEach(populationName => {
-                            const population = categoryData.getPopulation(populationName);
-                            const yValue = multiplePopulations ? entry.metrics[metricSlug][populationName] : entry.metrics[metricSlug];
-                            const dataPoint = new DataPoint(entry.date, yValue);
-                            population.addDataPoint(dataPoint);
-                        }); // For each population NAME
-
-                    } else if (metricType === 'table') {
-                        // For each row NAME
-                        Object.keys(entry.metrics[metricSlug]).map(rowName => {
-                            const tableSnapshot = categoryData.getTableSnapshot(entry.date);
-                            const row = new Row(rowName, entry.metrics[metricSlug][rowName]);
-                            tableSnapshot.addRow(row);
-                        }); // For each row NAME
-                    }
-
-                }); // For each metric NAME
-
-            }); // For each entry OBJECT
-
-        }); // For each category NAME
-
-        callback(dataset.render());
-
-    } // processCategories
-}
 
 class Dataset {
     constructor(title, description, defaultCategory) {
