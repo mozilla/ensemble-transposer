2e2b24265e40b057bb35fd79d36b7a0370451af8
John Karahalis <john.karahalis@gmail.com>
2018-11-19 14:47:38 -0500

    Rewrite, simplify comparison with async/await

--- tests/api-equivalence.test.js
+++ tests/api-equivalence.test.js
@@ -1,67 +1,47 @@
-const request = require('request');
+const request = require('request-promise-native');
 const chai = require('chai');
 const deepEqualInAnyOrder = require('deep-equal-in-any-order');
 
 chai.use(deepEqualInAnyOrder);
 
 
-function getJSON(url, cb) {
-    request(url, (err, response, body) => {
-        // eslint-disable-next-line no-console
-        if (err) return console.error(err);
-        cb(JSON.parse(body));
-    });
+async function getJSON(url) {
+    return JSON.parse(await request(url));
 }
 
-function getLocalJSON(path, cb) {
-    getJSON(`http://localhost:3000/datasets/${path}`, cb);
+async function getLocalJSON(path) {
+    return getJSON(`http://localhost:3000/datasets/${path}`);
 }
 
-function getProductionJSON(path, cb) {
-    getJSON(`https://ensemble-transposer.herokuapp.com/datasets/${path}`, cb);
+async function getProductionJSON(path) {
+    return getJSON(`https://ensemble-transposer.herokuapp.com/datasets/${path}`);
 }
 
 it('Local API output is equivalent to production API output', function(done) {
     this.timeout(50000);
 
-    const dashboards = [
-        'hardware',
-        'user-activity',
-        'usage-behavior',
-    ];
+    async function compare() {
+        const dashboards = [
+            'hardware',
+            'user-activity',
+            'usage-behavior',
+        ];
 
-    const dashboardPromises = [];
+        for (const dashboardName of dashboards) {
+            const localSummary = await getLocalJSON(dashboardName);
+            const productionSummary = await getProductionJSON(dashboardName);
+            chai.expect(productionSummary).to.deep.equalInAnyOrder(localSummary);
 
-    dashboards.forEach(dashboardName => {
-        dashboardPromises.push(new Promise(resolveDashboard => {
-            getLocalJSON(dashboardName, localSummary => {
-                const metricPromises = [];
+            for (const categoryName of localSummary.categories) {
+                for (const metricName of localSummary.metrics) {
+                    const metricPath = `${dashboardName}/${categoryName}/${metricName}`;
+                    const localMetric = await getLocalJSON(metricPath);
+                    const productionMetric = await getProductionJSON(metricPath);
+                    chai.expect(productionMetric).to.deep.equalInAnyOrder(localMetric);
+                }
+            }
+        }
+    }
 
-                metricPromises.push(new Promise(resolveSummary => {
-                    getProductionJSON(dashboardName, productionSummary => {
-                        chai.expect(productionSummary).to.deep.equalInAnyOrder(localSummary);
-                        return resolveSummary();
-                    });
-                }));
-
-                localSummary.categories.forEach(categoryName => {
-                    localSummary.metrics.forEach(metricName => {
-                        metricPromises.push(new Promise(resolveMetric => {
-                            const metricPath = `${dashboardName}/${categoryName}/${metricName}`;
-                            getLocalJSON(metricPath, localMetric => {
-                                getProductionJSON(metricPath, productionMetric => {
-                                    chai.expect(productionMetric).to.deep.equalInAnyOrder(localMetric);
-                                    return resolveMetric();
-                                });
-                            });
-                        }));
-                    });
-                });
-
-                Promise.all(metricPromises).then(() => resolveDashboard());
-            });
-        }));
-    });
-
-    Promise.all(dashboardPromises).then(() => done());
+    compare().then(done).catch(done);
 });
